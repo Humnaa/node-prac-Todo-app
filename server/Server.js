1bb68307f9af7911936b426a10a471cdb275284e
Humna <humna.aleem@southvillesol.com>
2019-05-29 15:51:59 +0500

    validators and default values

--- server/Server.js
+++ server/Server.js
@@ -1,24 +1,40 @@
 var mongoose = require("mongoose");
+var validator = require("validator");
 
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/Todo-app");
 
 var User = mongoose.model("User", {
   name: {
-    type: String
+    type: String,
+    required: true,
+    trim: true,
+    minlenght: 1
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: "{VALUE} is not a valid email"
+    }
   },
   completed: {
-    type: Boolean
-  }
-  ,
+    type: Boolean,
+    default: false
+  },
   age: {
     type: Number
   }
 });
 var newUser = new User({
-  name: "Rehan",
-  completed:true,
-  age:30
+  name: "Humna",
+  email: "humna.aleem111@gmail.com",
+  completed: true,
+  age: 30
 });
 newUser.save().then(
   doc => {
